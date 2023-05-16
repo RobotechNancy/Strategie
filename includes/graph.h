19ca4c6862f04e2521f4e22a0254819c0f2eb097
mrspaar <julienpistre.cagnes@gmail.com>
2023-05-16 22:38:04 +0200

    Réduction des dépendances

--- includes/graph.h
+++ includes/graph.h
@@ -1,8 +1,7 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <robotech/can.h>
-#include <robotech/xbee.h>
+#include "robotech/logs.h"
 
 
 class Graph {
@@ -11,17 +10,15 @@ private:
     char **steps;
     char step_count;
 
-    Logger *logs;
+    Logger logs;
     std::mutex mut;
 public:
-    explicit Graph(Logger *logs, char step_count);
+    explicit Graph(char step_count);
 
     Graph &set_step(char step);
     Graph &clear_step(char step);
     Graph &set_prev(char step, char* prev);
-
     bool can_switch(char from, char to);
-    void send_state(Can &can, CAN_ADDR addr);
 };
 
 #endif
--- src/graph.cpp
+++ src/graph.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 
-Graph::Graph(Logger *logs, char step_count) {
-    this->state = new bool[step_count];
+Graph::Graph(char count): logs("system") {
+    state = new bool[step_count];
 
     for (int i = 0; i < step_count; i++)
-        this->state[i] = false;
+        state[i] = false;
 
-    this->steps = new char*[step_count];
-    this->step_count = step_count;
-    this->logs = logs;
+    steps = new char*[step_count];
+    step_count = count;
 }
 
 
@@ -21,7 +20,7 @@ Graph &Graph::set_step(char step) {
     state[step-1] = true;
     mut.unlock();
 
-    *logs << "Etape " << int(step) << " activée" << mendl;
+    logs << "Etape " << int(step) << " activée" << mendl;
     return *this;
 }
 
@@ -31,7 +30,7 @@ Graph &Graph::clear_step(char step) {
     state[step-1] = false;
     mut.unlock();
 
-    *logs << "Etape " << int(step) << " désactivée" << mendl;
+    logs << "Etape " << int(step) << " désactivée" << mendl;
     return *this;
 }
 
@@ -48,13 +47,13 @@ bool Graph::can_switch(char from, char to) {
     mut.lock();
 
     if (!state[from-1]) {
-        *logs << "Demande refusée (pas d'étape n°" << int(from) << ")" << mendl;
+        logs << "Demande refusée (pas d'étape n°" << int(from) << ")" << mendl;
         mut.unlock();
         return false;
     }
 
     if (steps[to-1] == nullptr) {
-        *logs << "Demande refusée (pas de prérequis pour l'étape n°" << int(to) << ")" << mendl;
+        logs << "Demande refusée (pas de prérequis pour l'étape n°" << int(to) << ")" << mendl;
         mut.unlock();
         return false;
     }
@@ -63,7 +62,7 @@ bool Graph::can_switch(char from, char to) {
 
     while (steps[to-1][i] != 0) {
         if (steps[to-1][i] == from) {
-            *logs << "Demande acceptée (étape n°" << int(from) << " -> étape n°" << int(to) << ")" << mendl;
+            logs << "Demande acceptée (étape n°" << int(from) << " -> étape n°" << int(to) << ")" << mendl;
             mut.unlock();
             return true;
         }
@@ -71,24 +70,7 @@ bool Graph::can_switch(char from, char to) {
         i++;
     }
 
-    *logs << "Demande refusée (étape n°" << int(from) << " -> étape n°" << int(to) << ")" << mendl;
+    logs << "Demande refusée (étape n°" << int(from) << " -> étape n°" << int(to) << ")" << mendl;
     mut.unlock();
     return false;
-}
-
-
-void Graph::send_state(Can &can, CAN_ADDR addr) {
-    mut.lock();
-
-    unsigned int len = step_count/8 + (step_count%8==0? 0:1);
-    auto *data = new unsigned char[len];
-
-    for (int i=0; i < len*8; i++)
-        if (i < step_count && state[i])
-            data[i/8] |= 1 << (i % 8);
-        else
-            data[i/8] &= ~(1 << (i%8));
-
-    can.send(addr, FCT_CHANGEMENT_ETAT, data, len, false, 0, 0);
-    mut.unlock();
 }
